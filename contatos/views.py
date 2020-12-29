c0a0026e77ba848aacae04e07cce169a13ed2047
Leonard107 <leonardcampelo107@gmail.com>
2020-12-29 17:05:37 -0300

    criando campo de pesquisa

--- contatos/views.py
+++ contatos/views.py
@@ -7,7 +7,7 @@ from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
-    #contatos = Contato.objects.all()
+    # contatos = Contato.objects.all()
     contatos = Contato.objects.order_by('-id')
     paginator = Paginator(contatos, 5)
 
@@ -20,7 +20,7 @@ def index(request):
 
 
 def ver_contato(request, contato_id):
-    #contato = Contato.objects.get(id=contato_id)
+    # contato = Contato.objects.get(id=contato_id)
     contato = get_object_or_404(Contato, id=contato_id)
 
     if not contato.mostrar:
@@ -28,4 +28,17 @@ def ver_contato(request, contato_id):
 
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
+    })
+
+
+def busca(request):
+    # contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('-id')
+    paginator = Paginator(contatos, 5)
+
+    page_number = request.GET.get('page')
+    contatos = paginator.get_page(page_number)
+
+    return render(request, 'contatos/busca.html', {
+        'contatos': contatos
     })
